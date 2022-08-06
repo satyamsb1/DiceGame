625b324e3281a23e9ad8486fdfec1ea55e757ba2
Satyam Borade <68340284+satyamsb1@users.noreply.github.com>
2022-08-06 20:47:01 +0530

    update
    added more functionallity using dom manupilations

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
-var randomNumber1=Math.random();
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 
-randomNumber1=Math.floor(randomNumber1*6)+1;//1-->6
-
-var randomDiceImage="dice"+randomNumber1+".png"; // dice1.png-->dice6.png
-
-var randomImageSource="images/"+randomDiceImage; //images/dice1.png-->images/dice6.png
-
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-var randomNumber2=(Math.floor(Math.random()*6)+1);
-
-var ramdomDiceImage2="dice"+ randomNumber2+".png";
-
-var randomImageSource2="images/"+ramdomDiceImage2;
-
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSource2);
-
-if(randomNumber1>randomNumber2)
+function makeSound(key)
 {
-    document.querySelector("h1").innerHTML="Player 1 wins";
+    switch (key) {
+        case "w":
+            var audio= new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+            
+        case "a":
+            var audio= new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+
+        case "s":
+            var audio= new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+
+        case "d":
+            var audio= new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+
+        case "j":
+            var audio= new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+
+        case "k":
+            var audio= new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+
+        case "l":
+            var audio= new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+            
+        default:
+            console.log(key);
+            break;
+    }
 }
-else if(randomNumber1===randomNumber2)
+function buttonAnimation(currentKey)
 {
-    document.querySelector("h1").innerHTML="Draw";
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+   setTimeout( function()
+   {
+    activeButton.classList.remove("pressed")
+   } ,100);
+    
+
 }
-else
+
+for(var i=0; i<numberOfDrumButtons; i++)
 {
-    document.querySelector("h1").innerHTML="Player 2 wins";
+    document.querySelectorAll("button")[i].addEventListener("click", function()
+    {
+
+        var buttonInnerHTML=this.innerHTML;
+        
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });   
+    //handleClick Function is called when button is clicked
 }
+
+document.addEventListener("keydown", function(event)
+{
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
